359569803659f36efcca7b26fb3e86607d9d7b73
prajita11 <timprajita100@gmail.com>
2024-09-01 21:08:45 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const name="prajita"
+  return (
+    <>
+   <h1>Hello people</h1>
+   <h2>Welcome to Prajita's Website</h2>
+   <h3>Excited!!</h3>
+   </>
+  )
+}
+
+export default App
